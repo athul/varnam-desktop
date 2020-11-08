f2b239b6020c587b78cf5c7b31ca9ceb0642424a
Subin Siby <mail@subinsb.com>
2020-11-08 14:19:37 +0530

    Make suggestions dir too

--- windows-setup.bat
+++ windows-setup.bat
@@ -1,1 +1,12 @@
-CheckNetIsolation.exe LoopbackExempt -a -n="Microsoft.Win32WebViewHost_cw5n1h2txyewy" 
+CheckNetIsolation.exe LoopbackExempt -a -n="Microsoft.Win32WebViewHost_cw5n1h2txyewy"
+
+SET VARNAM_SYMBOLS_DIR=%APPDATA%\varnam\vst
+SET VARNAM_SUGGESTIONS_DIR=%APPDATA%\varnam\suggestions
+
+IF NOT EXIST "%VARNAM_SYMBOLS_DIR%" (
+  mkdir "%VARNAM_SYMBOLS_DIR%"
+)
+
+IF NOT EXIST "%VARNAM_SUGGESTIONS_DIR%" (
+  mkdir "%VARNAM_SUGGESTIONS_DIR%"
+)
